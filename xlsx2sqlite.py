1f1c776c617762e0bb4c75345c6aba6e1deb4fc1
smhyzh <yangzhenghongdee@163.com>
2019-03-07 11:04:41 +0800

    第一个实现.xlsx文件转换到sqlite数据库文件的版本.

--- xlsx2sqlite.py
+++ xlsx2sqlite.py
@@ -2,8 +2,8 @@
 #-*- coding:utf-8 -*-
 # Author:smh
 
+from dbopt import MyDateBase
 from openpyxl import load_workbook
-
 from xlsxheader import XlsxHeader
 
 
@@ -15,11 +15,19 @@ class XlsxToSqlite():
         '''
         self._wb = work_book
 
-    def convertToSqlite(self,table_label,sqlite_db_path=None):
+    def convertToSqlite(self,table_label,table_name=None,sqlite_db_path=None):
         '''
+        Convert xlsx to sqlite database will lost cells style information.
         '''
         if table_label not in self._wb.sheetnames:
             raise ValueError('table_label:{} not found.'.format(table_label))
+
+        if not sqlite_db_path:
+            sqlite_db_path = 'test.db'
+
+        if not table_name:
+            table_name = table_label
+
         ws = self._wb[table_label]
 
         xhdr = XlsxHeader(ws)
@@ -29,7 +37,16 @@ class XlsxToSqlite():
             print('not found table items.\nconvert to sqlite failed!')
         
         items_list = [ item.value for item in xhdr.getLine(item_index)]
-        print(items_list)
+
+        mdbs = MyDateBase(sqlite_db_path)
+        with mdbs as mdb:
+            sql_str = mdb.constructHeaderSQL(table_name,items_list)
+            print(sql_str)
+            mdb.runSQL(sql_str)
+            for every_row in list(ws.rows)[item_index+1:]:
+                row_data = [item.value for item in every_row]
+                sql_str = mdb.insertRowSQL(row_data)
+                mdb.runSQL(sql_str,row_data)
 
 
 if __name__ == "__main__":
